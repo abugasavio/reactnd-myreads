fd2a59ab93ccbf5ffe48a6a1e81351f82310759b
Savio Abuga <savioabuga@gmail.com>
2017-10-04 21:24:45 +0300

    style: add semicolons to js files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Route } from 'react-router-dom';
-import './App.css'
-import Search from './Search';
-import Home from './Home';
+import React from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import Search from "./Search";
+import Home from "./Home";
 
 class BooksApp extends React.Component {
   render() {
     return (
-			<div className="app">
-				<Route exact path="/" component={Home} />
-				<Route path="/search" component={Search} />
-			</div>
-		)
-	}
+      <div className="app">
+        <Route exact path="/" component={Home} />
+        <Route path="/search" component={Search} />
+      </div>
+    );
+  }
 }
 
-export default BooksApp
+export default BooksApp;
